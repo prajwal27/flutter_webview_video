3d814a603f0cf7350fb751a313b193d8d749e876
prajwal27 <prajwalsbelagavi.171co229@nitk.edu.in>
2020-05-31 14:47:32 +0530

    webview controller

--- lib/features/presentation/pages/shop_page.dart
+++ lib/features/presentation/pages/shop_page.dart
@@ -1,18 +1,75 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ShopPage extends StatelessWidget {
+class ShopPage extends StatefulWidget {
   const ShopPage({Key key}) : super(key: key);
 
   @override
+  _ShopPageState createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  String _title = 'Himdeve Shop';
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  Widget _buildChangeTitleBtn() {
+    return FloatingActionButton(
+      onPressed: () {
+        setState(() {
+          _title = 'Himdeve Development Tutorial';
+        });
+      },
+      child: Icon(Icons.link),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return _buildWebView();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Himdeve Shop'),
+      ),
+      body: _buildWebView(),
+      floatingActionButton: _buildShowUrlBtn(),
+    );;
   }
 
   Widget _buildWebView() {
     return WebView(
       javascriptMode: JavascriptMode.unrestricted,
       initialUrl: 'https://himdeve.eu',
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+      },
+    );
+  }
+
+  Widget _buildShowUrlBtn() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async {
+              var url = await controller.data.currentUrl();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                    content: Text(
+                      'Current url is: $url',
+                      style: TextStyle(fontSize: 20),
+                    )),
+              );
+            },
+            child: Icon(Icons.link),
+          );
+        }
+
+        return Container();
+      },
     );
   }
 }
